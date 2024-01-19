888aae0bb6ef02428c1d541d5df8628cf6dda10b
Mr Groot <113226362+MrGrootx@users.noreply.github.com>
2024-01-19 10:43:30 +0530

    feat: navbar added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Roboto: ["Roboto Mono", "monospace"],
+      },
+      colors: {
+        darkgray: "#222831",
+        graywhite: "#EEEEEE",
+        lightgray: "#393E46",
+        ctmblue: "#00ADB5",
+        graywhite: "#EEEEEE",
+      },
+    },
   },
   plugins: [],
-}
-
+};
